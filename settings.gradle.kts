ab9fd06a00056139f3362eddcad6b9ff8f55ec92
projectsijaga-update <projectsijaga@gmail.com>
2025-12-05 16:21:01 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,22 @@ dependencyResolutionManagement {
 
 rootProject.name = "Teleconizer"
 include(":app")
+
+// --- FIX KHUSUS AARCH64 (KALI LINUX/ARM) ---
+// Kode ini aman untuk GitHub Actions (x86) karena hanya akan aktif
+// jika Gradle meminta aapt2. GitHub akan pakai versi defaultnya,
+// sedangkan Kali Linux Anda akan dipaksa pakai versi linux-aarch64.
+gradle.lifecycle.beforeProject {
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "com.android.tools.build" && requested.name == "aapt2") {
+                // Deteksi Arsitektur Sistem Operasi
+                val osArch = System.getProperty("os.arch")
+                if (osArch == "aarch64" || osArch.startsWith("arm")) {
+                    println(">>> SYSTEM ARM64 DETECTED: FORCE AAPT2 LINUX-AARCH64")
+                    useTarget("com.android.tools.build:aapt2:${requested.version}:linux-aarch64")
+                }
+            }
+        }
+    }
+}
