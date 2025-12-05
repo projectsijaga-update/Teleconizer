60b231000c164ae40ba0c91c9fc5d13dea227c63
projectsijaga-update <projectsijaga@gmail.com>
2025-12-05 16:22:04 +0700

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file compatible with AGP 8.12.3 (Gradle 8.10+) and Java 21
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.google.gms.google.services) apply false
-}
